e76d8b9319d2714b25b5c936e5aad7c0d35dbaf2
nagi <ryuto65536@icloud.com>
2022-06-06 08:37:32 +0900

    torima

--- control.py
+++ control.py
@@ -1,7 +1,13 @@
 # 交差点で制御するプログラム
 
+import datetime
 import json
+from tabnanny import check
+import time
+from typing import Any
+import pytz
 import socket
+import sqlite3
 from concurrent.futures import ThreadPoolExecutor
 
 IPADDR: str = "127.0.0.1"
@@ -11,22 +17,47 @@ sock_sv: socket.socket = socket.socket(socket.AF_INET)
 sock_sv.bind((IPADDR, PORT))
 sock_sv.listen()
 
+conn = sqlite3.connect('./db/crossroads.db', isolation_level=None)
+cur = conn.cursor()
+
+cur.execute(f'''CREATE TABLE IF NOT EXISTS crMain(
+    time        INTEGER,
+    carId       INTEGER PRIMARY KEY AUTOINCREMENT,
+    from_        TEXT,
+    to_          TEXT,
+    condition   TEXT,
+    completion  TEXT
+)''')
+
+cur.execute(f'''CREATE TABLE IF NOT EXISTS crTag(
+    trafficId TEXT,
+    from_N TEXT, from_E TEXT, from_S TEXT, from_W TEXT,
+    stop_N TEXT, stop_E TEXT, stop_S TEXT, stop_W TEXT,
+    to_N   TEXT, to_E   TEXT, to_S   TEXT, to_W   TEXT
+)''')
+
+
 # データ受信関数
-def recv_client(sock: socket.socket) -> None:
-    while True:
-        try:
-            data_encode: bytes = sock.recv(1024)
-            # 受信データ0バイト時は接続終了
-            if data_encode == b"":
-                break
+def recv_client(sock: socket.socket, addr: tuple[Any]) -> None:
+    print(f'-connected {addr}')
+    
+    data_encode: bytes = sock.recv(1024)
+    data_decode: str = data_encode.decode('utf-8')
+    data = json.loads(data_decode)
+    # now: str = datetime.datetime.now(pytz.timezone('Asia/Tokyo')).strftime('%H:%M.%f')
+    print(data)
+    wtd = 'stop'
+    while wtd == 'stop':
+        wtd = input('what to do?\n> ')
+        if wtd == 'stop':
+            sock.send('stop'.encode('utf-8'))
+        else:
+            sock.send('ok'.encode('utf-8'))
 
-            data_decode: str = data_encode.decode('utf-8')
-            data = json.loads(data_decode)
-            print(data)
-
-        # 切断時の例外を捕捉したら終了
-        except ConnectionResetError:
-            break
+    data_encode: bytes = sock.recv(1024)
+    data_decode: str = data_encode.decode('utf-8')
+    data = json.loads(data_decode)
+    print(data)
     
     # クライアントをクローズ処理
     sock.shutdown(socket.SHUT_RDWR)
@@ -39,4 +70,4 @@ while True:
     sock_cl, addr = sock_sv.accept()
 
     executor = ThreadPoolExecutor(max_workers=1)
-    executor.submit(recv_client, sock_cl)
+    executor.submit(recv_client, sock_cl, addr)
--- tmp_client.py
+++ tmp_client.py
@@ -14,7 +14,7 @@ sock.connect((IPADDR, PORT))
 print('⚡ tmp_client.py start')
 while True:
     input('> ')
-    data = {'carid': str(CAR_ID)}
+    data = {'carid': str(CAR_ID), 'condition': 'connect', 'tagId': 'abc123'}
     data_json: str = json.dumps(data)
     data_encode: bytes = data_json.encode('utf-8')
 
@@ -22,6 +22,18 @@ while True:
         sock.send(data_encode)
     except ConnectionResetError:
         break
+
+    data_decode: str = 'stop'
+    while data_decode == 'stop':
+        data_encode: bytes = sock.recv(1024)
+        data_decode: str = data_encode.decode('utf-8')
+        print(data_decode)
+
+    input('> ok?')
+    data = {'carid': str(CAR_ID), 'condition': 'passed', 'tagId': 'def123'}
+    data_json: str = json.dumps(data)
+    data_encode: bytes = data_json.encode('utf-8')
+    sock.send(data_encode)
 
 # 送受信の切断
 sock.shutdown(socket.SHUT_RDWR)
