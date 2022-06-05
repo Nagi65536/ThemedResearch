e5b58c06c0851f720460c8c435deea24c406d555
nagi <ryuto65536@icloud.com>
2022-06-05 20:16:08 +0900

    tcp通信マルチプロセス完了

---
+++ memo/tcp/server_cfutures.py
@@ -0,0 +1,37 @@
+import socket
+from concurrent.futures import ThreadPoolExecutor
+
+IPADDR: str = "127.0.0.1"
+PORT: int = 7776
+
+sock_sv: socket.socket = socket.socket(socket.AF_INET)
+sock_sv.bind((IPADDR, PORT))
+sock_sv.listen()
+
+# データ受信ループ関数
+def recv_client(sock: socket.socket, addr: tuple[str]) -> None:
+    while True:
+        try:
+            data = sock.recv(1024)
+            # 受信データ0バイト時は接続終了
+            if data == b"":
+                break
+            print(data.decode("utf-8"))
+        # 切断時の例外を捕捉したら終了
+        except ConnectionResetError:
+            break
+    
+    # クライアントをクローズ処理
+    sock.shutdown(socket.SHUT_RDWR)
+    sock.close()
+
+i: int = 1
+# クライアント接続ループ
+while True:
+    # クライアントの接続受付
+    sock_cl, addr = sock_sv.accept()
+
+    executor = ThreadPoolExecutor(max_workers=i)
+    executor.submit(recv_client, sock_cl, addr)
+    
+    i += 1
