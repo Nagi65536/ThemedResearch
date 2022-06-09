28acbf3d8eddecbc1278fbd93ce5c0a761f5d4ea
nagi <ryuto65536@icloud.com>
2022-06-09 23:19:01 +0900

    とりまできた 複数きた時inputだめ

--- memo/tcp/client_mprocess.py
+++ memo/tcp/client_mprocess.py
@@ -1,24 +1,66 @@
 import socket
+import json
+import random
+from typing import Any
 
-IPADDR = "127.0.0.1"
-PORT = 7776
+IPADDR: str = "127.0.0.1"
+PORT: int = 7777
+CAR_ID: str = str(random.randint(10000, 99999))
 
 sock: socket.socket = socket.socket(socket.AF_INET)
 sock.connect((IPADDR, PORT))
 
-while True:
-    # 任意の文字を入力
-    data: str = input("> ")
-    # exitを切断用コマンドとしておく
-    if data == "exit":
-        break
-    else:
-        try:
-            sock.send(data.encode("utf-8"))
-        except ConnectionResetError:
+
+def main() -> None:
+    while True:
+        # 任意の文字を入力
+        data: str = input("> ")
+        # exitを切断用コマンドとしておく
+        condition: str = ""
+        if data == "1":
+            condition: str = "entry"
+            print("entry")
+        elif data == "2":
+            print(stop)
+            condition: str = "stop"
+        elif data == "3":
+            print("passing")
+            condition: str = "passing"
+
+        send_data: dict[str, Any] = {"carId": CAR_ID, "condition": condition}
+        send_data_json = json.dumps(send_data)
+        send_data_encode: bytes = send_data_json.encode("utf-8")
+
+        result: int = socket_send(send_data_encode)
+
+        if result:
             break
 
-# 送受信の切断
-sock.shutdown(socket.SHUT_RDWR)
-# ソケットクローズ
-sock.close()
+        if data == "1" or data == "2":
+            data: str = socket_get()
+            print(data)
+
+
+def socket_send(send_data_encode: bytes) -> int:
+    try:
+        sock.send(send_data_encode)
+        return 0
+    except ConnectionResetError:
+        # 送受信の切断
+        sock.shutdown(socket.SHUT_RDWR)
+        # ソケットクローズ
+        sock.close()
+        return 1
+
+def socket_get():
+    print('待機中')
+    get_data: bytes = sock.recv(1024)
+    data_json_decode: str = get_data.decode("utf-8")
+    data: dict[str, Any] = json.loads(data_json_decode)
+    print(data)
+    
+    return data
+
+print('⚡ client_mprocess.py start')
+if __name__ == "__main__":
+    main()
--- memo/tcp/server_cfutures.py
+++ memo/tcp/server_cfutures.py
@@ -1,23 +1,61 @@
 import socket
+import json
 from concurrent.futures import ThreadPoolExecutor
+from typing import Any
+from numpy import byte
 
 IPADDR: str = "127.0.0.1"
-PORT: int = 7776
+PORT: int = 7777
 
 sock_sv: socket.socket = socket.socket(socket.AF_INET)
 sock_sv.bind((IPADDR, PORT))
 sock_sv.listen()
 
+def main() -> None:
+    while True:
+        # クライアントの接続受付
+        sock_cl, addr = sock_sv.accept()
+
+        executor = ThreadPoolExecutor(max_workers=1)
+        executor.submit(recv_client, sock_cl, addr)
+
+
 # データ受信ループ関数
 def recv_client(sock: socket.socket, addr: tuple[str]) -> None:
+    print(f"-connected {addr}")
+
     while True:
         try:
-            data = sock.recv(1024)
-            # 受信データ0バイト時は接続終了
-            if data == b"":
+            data: str = socket_get(sock)
+            car_id: str = data["carId"]
+            condition: str = data["condition"]
+            
+            if condition == "entry":
+                print("entry - go, stop")
+                instruction = input("> ")
+
+                send_data: dict[str, Any] = {"carId": car_id, "instruction": instruction}
+                send_data_json = json.dumps(send_data)
+                send_data_encode: bytes = send_data_json.encode("utf-8")
+
+                result: int = socket_send(sock, send_data_encode)
+                if result:
+                    break
+
+            elif condition == "stop":
+                print("stop - go")
+                input("> ")
+                send_data: dict[str, Any] = {"carId": car_id, "instruction": "go"}
+                send_data_json = json.dumps(send_data)
+                send_data_encode: bytes = send_data_json.encode("utf-8")
+
+                result: int = socket_send(sock, send_data_encode)
+                if result:
+                    break
+            
+            else:
                 break
-            print(data.decode("utf-8"))
-        # 切断時の例外を捕捉したら終了
+
         except ConnectionResetError:
             break
     
@@ -26,10 +64,30 @@ def recv_client(sock: socket.socket, addr: tuple[str]) -> None:
     sock.close()
 
 
-# クライアント接続ループ
-while True:
-    # クライアントの接続受付
-    sock_cl, addr = sock_sv.accept()
+def socket_send(sock: socket.socket, send_data_encode: bytes) -> int:
+    try:
+        print('ok')
+        sock.send(send_data_encode)
+        print('send')
+        return 0
 
-    executor = ThreadPoolExecutor(max_workers=1)
-    executor.submit(recv_client, sock_cl, addr)
+    except ConnectionResetError:
+        # 送受信の切断
+        sock.shutdown(socket.SHUT_RDWR)
+        # ソケットクローズ
+        sock.close()
+        return 1
+
+
+def socket_get(sock: socket.socket) -> dict[str, Any]:
+    print("待機中")
+    get_data: bytes = sock.recv(1024)
+    data_json_decode: str = get_data.decode("utf-8")
+    data: dict[str, Any] = json.loads(data_json_decode)
+    
+    return data
+    
+
+print('⚡ server_cfutures.py start')
+if __name__ == "__main__":
+    main()
