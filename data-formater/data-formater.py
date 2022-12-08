9b0f5d599afec9431c782194d7af572221a886c5
SatooRu65536 <ryuto4619@gmail.com>
2022-12-08 11:00:34 +0900

    Merge branch 'main' of https://github.com/SatooRu65536/ThemedResearch

--- data-formater/data-formater.py
+++ data-formater/data-formater.py
@@ -1,22 +1,27 @@
-datum = []
-range_list = []
+import glob
 
-with open('./input.log') as f:
-    datum_list = f.read().split('\n')
+files = glob.glob("logs/*")
 
-for i in range(len(datum_list)):
-    if i % 2 == 0 and i+2 <= len(datum_list):
-        range_list.append(datum_list[i])
-        datum.append([datum_list[i], datum_list[i+1]])
+for file in files:
+    datum = []
+    range_list = []
 
-range_list = list(set(range_list))
+    with open(file) as f:
+        datum_list = f.read().split('\n')
 
-input_data = ''
-for range_ in range_list:
-    input_data += range_ + '\n'
-    for data in datum:
-        if range_ == data[0]:
-            input_data += data[1] + '\n'
+    for i in range(len(datum_list)):
+        if i % 2 == 0 and i+2 <= len(datum_list):
+            range_list.append(datum_list[i])
+            datum.append([datum_list[i], datum_list[i+1]])
 
-with open('./output.log', 'w') as f:
-    f.write(input_data)
+    range_list = list(set(range_list))
+
+    input_data = ''
+    for range_ in range_list:
+        input_data += range_ + '\n'
+        for data in datum:
+            if range_ == data[0]:
+                input_data += data[1] + '\n'
+
+    with open(f'./output-{file}', 'w') as f:
+        f.write(input_data)
